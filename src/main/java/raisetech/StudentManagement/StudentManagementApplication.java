072c764b5f8a5d5248489de8c2fc2dcfa94da85b
Ogawa1019 <ogawa20021019@gmal.com>
2026-06-02 22:37:11 +0900

    GETとPOSTを使用した学生情報管理機能を追加

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -1,20 +1,40 @@
 package raisetech.StudentManagement;
 
+import java.util.HashMap;
+import java.util.Map;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class StudentManagementApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(StudentManagementApplication.class, args);
+  private Map<String, String> student = new HashMap<>();
+
+  public StudentManagementApplication() {
+    student.put("name", "Ogawa Takashi");
+    student.put("age", "23");
   }
 
-  @GetMapping("/hello")
-  public String hello() {
-    return "Hello,World！";
+  public static void main(String[] args) {
+		SpringApplication.run(StudentManagementApplication.class, args);
+  }
+
+  @GetMapping("/studentInfo")
+  public Map<String, String> getStudentInfo() {
+    return student;
+  }
+
+  @PostMapping("/studentInfo")
+  public void setStudentInfo(
+      @RequestParam String name,
+      @RequestParam String age) {
+
+    student.put("name", name);
+    student.put("age", age);
   }
 }
