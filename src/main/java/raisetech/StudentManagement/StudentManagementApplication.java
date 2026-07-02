7998ee597371ad265ec7c17b7f8866631dcdd660
Ogawa1019 <ogawa20021019@gmal.com>
2026-07-02 18:17:27 +0900

    第10回演習課題

--- src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
+++ src/main/java/raisetech/StudentManagement/StudentManagementApplication.java
@@ -1,57 +1,30 @@
 package raisetech.StudentManagement;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class StudentManagementApplication {
 
-  private Map<String, String> student = new HashMap<>();
-
-  public StudentManagementApplication() {
-    student.put("name", "Ogawa Takashi");
-    student.put("age", "23");
-  }
+  @Autowired
+  private StudentRepository repository;
 
   public static void main(String[] args) {
 		SpringApplication.run(StudentManagementApplication.class, args);
   }
 
-  @GetMapping("/studentInfo")
-  public Map<String, String> getStudentInfo() {
-    return student;
+  @GetMapping("/studentList")
+  public List<Student> getStudentList(){
+    return repository.search();
   }
 
-  @PostMapping("/studentInfo")
-  public void setStudentInfo(
-      @RequestParam String name,
-      @RequestParam String age) {
-
-    student.put("name", name);
-    student.put("age", age);
+  @GetMapping("/studentCourseList")
+  public List<StudentCourse> getStudentCourseList() {
+    return repository.searchStudentCourse();
   }
-
-  @PutMapping("/studentInfo")
-  public void updateStudentInfo(
-      @RequestParam String name,
-      @RequestParam String age) {
-
-    student.put("name", name);
-    student.put("age", age);
-  }
-
-  @DeleteMapping("/studentInfo")
-  public void deleteStudentInfo() {
-    student.clear();
-  }
-
 }
