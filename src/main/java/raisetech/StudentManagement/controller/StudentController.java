716272c29aef74f61d9bdc7729d7473b3762a220
Ogawa1019 <ogawa20021019@gmal.com>
2026-07-10 14:48:34 +0900

    第14回演習課題

--- src/main/java/raisetech/StudentManagement/controller/StudentController.java
+++ src/main/java/raisetech/StudentManagement/controller/StudentController.java
@@ -2,15 +2,15 @@ package raisetech.StudentManagement.controller;
 
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 import raisetech.StudentManagement.controller.converter.StudentConverter;
 import raisetech.StudentManagement.data.Student;
 import raisetech.StudentManagement.data.StudentCourse;
-import raisetech.StudentManagement.domain.StudentDetail;
 import raisetech.StudentManagement.service.StudentService;
 
-@RestController
+@Controller
 public class StudentController {
 
   private final StudentService service;
@@ -23,13 +23,14 @@ public class StudentController {
   }
 
   @GetMapping("/studentList")
-  public List<StudentDetail> getStudentList() {
+  public String getStudentList(Model model) {
     List<Student> students = service.searchStudentList();
     List<StudentCourse> studentCourses = service.searchStudentCourseList();
 
-    return converter.convertStudentDetails(students, studentCourses);
+    model.addAttribute("studentList", converter.convertStudentDetails(students, studentCourses));
+    return "studentList";
   }
-  
+
   @GetMapping("/studentCourseList")
   public List<StudentCourse> getStudentCourseList() {
     return service.searchStudentCourseList();
--- src/main/java/raisetech/StudentManagement/service/StudentService.java
+++ src/main/java/raisetech/StudentManagement/service/StudentService.java
@@ -10,7 +10,7 @@ import raisetech.StudentManagement.repository.StudentRepository;
 @Service
 public class StudentService {
 
-  private StudentRepository repository;
+  private final StudentRepository repository;
 
   @Autowired
   public StudentService(StudentRepository repository) {
@@ -18,15 +18,10 @@ public class StudentService {
   }
 
   public List<Student> searchStudentList() {
-    return repository.search().stream()
-        .filter(student -> student.getAge() >= 30
-            && student.getAge() < 40)
-        .toList();
+    return repository.search();
   }
 
   public List<StudentCourse> searchStudentCourseList() {
-    return repository.searchStudentCourse().stream()
-        .filter(course -> course.getCourseName().equals("Javaコース"))
-        .toList();
+    return repository.searchStudentCourse();
   }
 }
